dd029d442c5135311d03f3347bb7c2e2d7ecf16b
Vinayak <vinayakdeepu@gmail.com>
2020-04-04 19:54:23 +0530

    Add functionality to fetch Auth0 certificate

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,30 +1,12 @@
 import 'source-map-support/register';
 import { CustomAuthorizerEvent, CustomAuthorizerHandler, CustomAuthorizerResult } from 'aws-lambda';
+import axios from 'axios';
 
 import { verify } from 'jsonwebtoken';
 import { JwtPayload } from '../../auth/JwtPayload';
 import { createLogger } from '../../utils/logger';
 
-const cert = `-----BEGIN CERTIFICATE-----
-MIIC/TCCAeWgAwIBAgIJLdY1rB5hLBchMA0GCSqGSIb3DQEBCwUAMBwxGjAYBgNV
-BAMTEWRpcmV2aW4uYXV0aDAuY29tMB4XDTIwMDMyNzE3NTkzNVoXDTMzMTIwNDE3
-NTkzNVowHDEaMBgGA1UEAxMRZGlyZXZpbi5hdXRoMC5jb20wggEiMA0GCSqGSIb3
-DQEBAQUAA4IBDwAwggEKAoIBAQC7lJxrAxJAAZknBiO7ml5l5Oce05d4jxC5/1dk
-pHAVdV3d8TeOS6WZMxfMSAx332Fy9hexOVDGTvHMH/7bM5pfu/zWKl9D0TYuMfqs
-VZJNuKt1vDYGa7LkOdrCibeSv9pwf+azUx+EM5xF/lVPV5rvxhDwdHPqGLv6N7hF
-wulCBI8PnTmlnQVjw99bF3kgK+ERKvN7haNyUR9B1R1sbV3kv9gdtjD+BBSfiiaM
-DAePxerOpTUG8lt5rL4WT93/88bDeLq1SrjOK4/7D2EhKHvm1ZjKKVtRAlF3UIbX
-0WRq1Ygz3Qs4c0yCBVMl186hk6h0fKb42pFQY8xwk+CYQPLRAgMBAAGjQjBAMA8G
-A1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFKtEJRO4S7nH48a8Y4WfNw//P6u7MA4G
-A1UdDwEB/wQEAwIChDANBgkqhkiG9w0BAQsFAAOCAQEAeOsOGaLdrf9QBJ+TH9kJ
-LzeFFRNSfEunKnEDn+Jd6h1ZCWcoTAWGiOG6krMLJ08vKYBiDZDgPEKz0ya6HCF9
-DpfzKoVyhEV7uSTkK0guC4Ar+PLiJtvdN2zArLDVDZeh9CHJuLnkIMLUM4Eh0h31
-J3p79GtgtR/rnWS1R7/Xr/SzXcJFpRRY6eLtK9mZfB9ZZZhD+GcOf0HwsVj3eovU
-6ureXDdSG9O5OnbFHFIk2/PPB1EBK0xUmtJB4+HfWP5WL3bBvjwsgCOGLJ4btd7Q
-moxsvNB4nQAMYia5SWRcPvvHbm5hMhyp9SRdbs+b5BBsa3cbvZitHqEiUjClw27Q
-Cw==
------END CERTIFICATE-----
-`;
+let auth0Cert;
 
 const logger = createLogger('Lambda:auth0');
  
@@ -32,7 +14,7 @@ export const handler: CustomAuthorizerHandler = async (event: CustomAuthorizerEv
     logger.info('Processing event', {event});
 
     try {
-        const decodedToken = verifyToken(event.authorizationToken);
+        const decodedToken = await verifyToken(event.authorizationToken);
         logger.info('User was authorized', {token: decodedToken});
         return {
             principalId: decodedToken.sub,
@@ -65,7 +47,7 @@ export const handler: CustomAuthorizerHandler = async (event: CustomAuthorizerEv
     }
 }
 
-function verifyToken(authHeader: string): JwtPayload {
+async function verifyToken(authHeader: string): Promise<JwtPayload> {
     if (!authHeader) 
         throw new Error('No authorization header');
 
@@ -73,5 +55,16 @@ function verifyToken(authHeader: string): JwtPayload {
         throw new Error('Invalid authorization header');
 
     const token = authHeader.split(' ')[1];
+    const cert = await fetchCertificate();
     return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload;
+}
+
+async function fetchCertificate(): Promise<string> {
+    if (!auth0Cert) {
+        console.log("Fetching Auth0 certificate");
+        const result = await axios.get(process.env.AUTH0_CERT_URL);
+        auth0Cert = result.data;
+        console.log("Certificate fetched", auth0Cert);
+    }
+    return auth0Cert;
 }
